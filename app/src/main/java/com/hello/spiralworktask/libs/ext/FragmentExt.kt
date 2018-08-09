6e6f30f7a74c4ad1a9861b0286e7cf37a1f5e1eb
Darick Roco <darick@creativehothouse.com>
2018-08-09 19:47:30 +0800

    Switch to alert dialog when showing login error

--- app/src/main/java/com/hello/spiralworktask/libs/ext/FragmentExt.kt
+++ app/src/main/java/com/hello/spiralworktask/libs/ext/FragmentExt.kt
@@ -4,6 +4,19 @@ import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
 import android.support.v4.app.Fragment
+import android.support.v7.app.AlertDialog
+
+fun Fragment.showAlertDialog(
+  title: String,
+  message: String
+) = activity
+    ?.let {
+      AlertDialog.Builder(it)
+          .setTitle(title)
+          .setMessage(message)
+          .setPositiveButton(android.R.string.ok, null)
+          .show()
+    }
 
 inline fun <reified T : ViewModel> Fragment.getViewModel(viewModelFactory: ViewModelProvider.Factory): T {
   return ViewModelProviders.of(this, viewModelFactory)[T::class.java]
